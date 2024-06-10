23df87cde95661a83199460419e5ac782e90c406
Jean Rojas <jean.rojas@usach.cl>
2024-06-10 19:01:27 -0400

    Desafio terminado

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const precioBase=400000;
+let cantidad=0;
+let totalPagar=0;
+document.querySelector("#precio").innerHTML=precioBase;
+document.querySelector("#cantidad").innerHTML = cantidad;
+document.querySelector("#totalPagar").innerHTML = totalPagar;
